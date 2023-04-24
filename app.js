e572c4189dbaafa45f5c371207d9acc474b42e7b
Hritik2023 <raihritik19898@gmail.com>
2023-04-24 17:33:24 +0530

    mess happen

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-var textInput = document.queryselector("#txt-input");
-var btnTranslate = document.queryselector("#btn-translate");
-var textoutput = document.queryselector("#txt-output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 
-var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
 
-function translatedUrl (textgiven){
-    var urlTranslate = serverUrl + "?" + "text=" + textgiven.value ;
-    return(urlTranslate);
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+
 }
 
+function clickHandler() {
+    var inputText = txtInput.value;
 
 
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+           })
 
-btnTranslate.addEventListener("click",function(){
-    console.log("Button clicked");
-    var UrlFinal = TranslaterUrl(textInput);
-    console.log(urlfinal);
-    fetch(urlFinal);
-    .then(Respone => Respone.json())
-    .then(json =>{ textoutput.innertext = json.contents.translated })
-    .catch(errorHandler);
 
-})
+           };
+
+        btnTranslate.addEventListener("click", clickHandler)
